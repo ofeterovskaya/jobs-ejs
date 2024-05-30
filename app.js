01483ad686d911f1586ee030f4b099bd85a05422
Oksana Fetter <fetter.oksana@gmail.com>
2024-05-29 20:15:29 -0400

    change styles, added boostrap, modify secretWord.ejs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require("dotenv").config(); // to load the .env file into the process.env object
 const session = require("express-session");
+
 // Initialize an express application
 const express = require('express');
 
@@ -12,10 +14,7 @@ app.set("view engine", "ejs");
 // Use body-parser middleware to parse incoming request bodies
 app.use(require("body-parser").urlencoded({ extended: true }));
 
-require("dotenv").config(); // to load the .env file into the process.env object
-
 const MongoDBStore = require('connect-mongodb-session')(session);
-
 const url = process.env.MONGO_URI;
 
 // Create a new MongoDBStore instance for storing sessions in MongoDB
@@ -30,6 +29,7 @@ const store = new MongoDBStore({
   });
   
   // the parameters for the session middleware
+  const inProduction = app.get("env") === "production";
   const sessionParms = {    
     secret: process.env.SESSION_SECRET,// The secret used to sign the session ID cookie    
     resave: true,// Forces the session to be saved back to the session store, even if the session was never modified during the request
@@ -38,13 +38,12 @@ const store = new MongoDBStore({
     
     store: store,// The store instance where sessions will be stored
     
-    cookie: { secure: false, sameSite: "strict" },// Options for the session ID cookie
+    cookie: { secure: false, sameSite: "strict", secure: inProduction  },// Options for the session ID cookie
   };
   
   // If the application is in production
-  if (app.get("env") === "production") {    
-    app.set("trust proxy", 1);// Trust the first proxy    
-    sessionParms.cookie.secure = true;// Serve secure cookies
+  if (inProduction) {    
+    app.set("trust proxy", 1); // trust first proxy       
   }
   
   // Use the session middleware with the defined parameters
@@ -54,7 +53,7 @@ const store = new MongoDBStore({
 
     app.get("/secretWord", (req, res) => {
         if (!req.session.secretWord) {
-          req.session.secretWord = "syzygy";
+            req.session.secretWord = "syzygy";
         }
         res.locals.info = req.flash("info");
         res.locals.errors = req.flash("error");
@@ -63,15 +62,15 @@ const store = new MongoDBStore({
 
     // Modify the POST route for /secretWord
     app.post("/secretWord", (req, res) => {
-    if (req.body.secretWord.toUpperCase()[0] == "P") {
-        // Set flash messages if the secret word starts with "P"
-        req.flash("error", "That word won't work!");
-        req.flash("error", "You can't use words that start with p.");
-    } else {
-        // Set the secret word in the session and set a flash message
-        req.session.secretWord = req.body.secretWord;
-        req.flash("info", "The secret word was changed.");
-    }
+        if (req.body.secretWord?.toLowerCase()?.startsWith("p")) {
+            // Set flash messages if the secret word starts with "P"
+            req.flash("error", "That word won't work!");
+            req.flash("error", "You can't use words that start with p.");
+        } else {
+            // Set the secret word in the session and set a flash message
+            req.session.secretWord = req.body.secretWord;
+            req.flash("info", "The secret word was changed.");
+        }
     // Redirect to the /secretWord route
         res.redirect("/secretWord");
     });
